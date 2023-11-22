dcabd9d2af7575b64ecd236e682a59cbe66917b1
Uliana Seredenko <uliana1331@gmail.com>
2023-11-22 21:25:48 +0200

    start 1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const handleStart = () => {
+  const bodyColor = getRandomHexColor();
+
+  timerID = setInterval(() => {
+    body.style.backgroundColor;
+  });
+};
+
+startBtn.addEventListener('click', handleStart);
