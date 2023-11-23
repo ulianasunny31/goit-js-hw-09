a4c442d16c83eeda7d5baaeb0e384530795f460e
Uliana Seredenko <uliana1331@gmail.com>
2023-11-23 09:59:47 +0200

    full first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
+let timerID;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -9,11 +10,35 @@ function getRandomHexColor() {
 }
 
 const handleStart = () => {
-  const bodyColor = getRandomHexColor();
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 
   timerID = setInterval(() => {
-    body.style.backgroundColor;
-  });
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const handleStop = () => {
+  clearInterval(timerID);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 };
 
 startBtn.addEventListener('click', handleStart);
+stopBtn.addEventListener('click', handleStop);
+
+//Styles for btns
+
+startBtn.style.padding = '10px 20px';
+startBtn.style.fontSize = '16px';
+startBtn.style.borderRadius = '5px';
+startBtn.style.color = 'green';
+startBtn.style.border = 'none';
+// startBtn.style.cursor = 'pointer';
+
+stopBtn.style.padding = '10px 20px';
+stopBtn.style.fontSize = '16px';
+stopBtn.style.borderRadius = '5px';
+stopBtn.style.color = 'red';
+stopBtn.style.border = 'none';
+// stopBtn.style.cursor = 'pointer';
