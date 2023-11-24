186d386719680f0df27a987b7ddcb64b3437ce2c
Uliana Seredenko <uliana1331@gmail.com>
2023-11-24 21:46:39 +0200

    03 старт

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('select[name="amount"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+      } else {
+      }
+    }, delay);
+  });
+}
+
+function createPromise() {
+  const delay = Number(delayInput.value, 10);
+  const step = Number(stepInput.value, 10);
+  const amount = Number(amountInput.value, 10);
+
+  7777788;
 }
