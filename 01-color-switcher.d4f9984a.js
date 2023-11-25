b713e9b508c9307465dbc35f59c02bb3b122248a
ulianasunny31 <125956896+ulianasunny31@users.noreply.github.com>
2023-11-25 08:54:47 +0000

    Deploying to gh-pages from @ ulianasunny31/goit-js-hw-09@b67ebb112819e97864715fcd2ec991fea9628889 🚀

---
+++ 01-color-switcher.d4f9984a.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=document.querySelector("body");e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.d4f9984a.js.map
