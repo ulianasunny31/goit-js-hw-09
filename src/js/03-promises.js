f3d2e5d24f278829d53aedaab2a783a70486ed0b
Uliana Seredenko <uliana1331@gmail.com>
2023-11-25 10:41:55 +0200

    full task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,17 @@
-const form = document.querySelector('.form');
-const delayInput = document.querySelector('input[name="delay"]');
-const stepInput = document.querySelector('input[name="step"]');
-const amountInput = document.querySelector('select[name="amount"]');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const selectors = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+};
+const options = {
+  position: 'right-bottom',
+  borderRadius: '5px',
+  cssAnimationStyle: 'from-top',
+  clickToClose: true,
+};
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -9,16 +19,38 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
+        resolve({ position, delay });
       } else {
+        reject({ position, delay });
       }
     }, delay);
   });
 }
 
-function createPromise() {
-  const delay = Number(delayInput.value, 10);
-  const step = Number(stepInput.value, 10);
-  const amount = Number(amountInput.value, 10);
+function createPromises(evt) {
+  evt.preventDefault();
 
-  7777788;
+  let delayValue = Number(selectors.delay.value);
+  let step = Number(selectors.step.value);
+  let amount = Number(selectors.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    let delayPromise = delayValue + step * i;
+
+    createPromise(i, delayPromise)
+      .then(({ position, delay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          options
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          options
+        );
+      });
+  }
 }
+
+selectors.form.addEventListener('submit', createPromises);
